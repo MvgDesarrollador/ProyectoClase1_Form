8f514f08a14579c9c26c6025699ec9271d7cbacb
Moises Mateo Villada Guevara <moises.villada@globant.com>
2024-04-30 11:24:55 -0500

    Se agregaron funcionalidades con javascript para capturar el valor de los campos del formulario y mostrarlos en consola

---
+++ controllers/controladorFormulario.js
@@ -0,0 +1,48 @@
+let cajaNombreProducto = document.getElementById('nombre');
+
+let cajaPrecioUnitario = document.getElementById('precioUnitario');
+
+let cajaDescripcion = document.getElementById('descripcion');
+
+let cajaFoto = document.getElementById('foto');
+
+let cajaReseña = document.getElementById('reseña');
+
+let botonFormulario = document.getElementById('botonFormulario');
+
+
+
+
+
+//Como detecto eventos
+
+//1. Una vez tenga una variable que almacena
+//la etiqueta sobre la cual quiero escuchar el evento
+
+
+//2. Utilizar la funcion o metodo propio de js (Watch Dog)
+
+//addEventListener Recibe dos parametros : cual evento?, que hago?
+
+botonFormulario.addEventListener("click", (event)=> {
+event.preventDefault();
+
+try {
+    let capturaNombre = cajaNombreProducto.value;
+    let capturaPrecioUnitario = cajaPrecioUnitario.value;
+    let capturaDescripcion = cajaDescripcion.value;
+    let capturaUrlFoto = cajaFoto.value;
+    let reseña = cajaReseña.value;
+
+    console.log(`Valor campo Nombre ${capturaNombre}`);
+    console.log(`Valor campo PrecioUnitario ${capturaNombre}`);
+    console.log(`Valor campo Descripcion ${capturaNombre}`);
+    console.log(`Valor campo CapturaUrlFoto ${capturaNombre}`);
+    console.log(`Valor campo Reseña ${capturaNombre}`);
+
+    
+} catch (error) {
+  console.log(`Se produjo un error: ${error}`);
+}
+
+});
